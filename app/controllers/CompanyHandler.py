e6632ad30f97ca0152d27cf1379e152b6040029e
Eladio Caritos <eladio@caritos.com>
2011-05-13 15:41:30 -0400

    Still working on the ajax query call. I've made it to the simplest element where it's not associated with the datastore query.

--- app/controllers/CompanyHandler.py
+++ app/controllers/CompanyHandler.py
@@ -8,9 +8,12 @@ from app.models.Company import Company
 
 class CompanyHandler(webapp.RequestHandler):
     def get(self):
-        name = cgi.escape(self.request.get('name'))
-        query = Company.get_by_key_name(name)
-        if query == None:
-            self.response.out.write("False")
-        else:
-            self.response.out.write("True")
+        # TODO: Querying the datastore still doesn't work.
+        # TODO: need to clean this up later after I get ajax call working
+        # name = cgi.escape(self.request.get('name'))
+        # query = Company.get_by_key_name(name)
+        #if query == None:
+        #     self.response.out.write("False")
+        #else:
+        #    self.response.out.write("True")
+        set.response.out.write("True")
--- app/models/Company.py
+++ app/models/Company.py
@@ -2,3 +2,11 @@ from google.appengine.ext import db
 
 class Company(db.Model):
     name = db.StringProperty(required=True)
+
+    def __init__(self):
+        # For debugging purposes, let's have google
+        # already defined in the database.
+        query = Company.get_by_key_name("google")
+        if query == None:
+            company = Company(name="google")
+            company.put()
